94b00e86aaddc4440147048679a25913243401f1
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-08 16:03:18 +0000

    Setting up beginning flow of CLI

--- lib/amazon10.rb
+++ lib/amazon10.rb
@@ -2,5 +2,5 @@ require "open-uri"
 require "nokogiri"
 
 require_relative "./amazon10/version"
-#require_relative "./amazon10/cli"
+require_relative "./amazon10/cli"
 #require_relative "./amazon10/books"
--- lib/amazon10/cli.rb
+++ lib/amazon10/cli.rb
@@ -1,2 +1,14 @@
 class Amazon10::CLI
+
+  def call
+    puts "Welcome to Amazon 10!"
+    list_books
+  end
+
+  def list_books
+    puts "These are the top 10 best selling books this week on Amazon:"
+    puts "books 1-10"
+    puts "Which would you like to see more information about?"
+  end
+
 end
